a1961158a764df8d842758d581390842fa57d916
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2023-02-18 15:13:05 +0300

    add check length

--- app/app.py
+++ app/app.py
@@ -2,14 +2,21 @@ import json
 
 from analyzer.model import NftScamModel
 from analyzer.analyzer import check_nft_token
+from app.analyzer.check_state import Response
 
 model_file = '/opt/ml/model'
 model = NftScamModel(model_file, device="cpu")
 
 
+def is_valid_token_length(token_address):
+    return len(token_address) == 44
+
+
 def lambda_handler(event, context):
     token_address = event['body']
-    result = check_nft_token(model, token_address)
+    result = Response.WRONG_INPUT
+    if is_valid_token_length(token_address):
+        result = check_nft_token(model, token_address)
 
     return {
         'statusCode': 200,
@@ -20,5 +27,3 @@ def lambda_handler(event, context):
             }
         )
     }
-
-
