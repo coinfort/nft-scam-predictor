b4776afd72b6c2b1dc45f1906a228ebd68cdb54b
Egor Bazhenov <egor.bazhenov@corp.mail.ru>
2023-02-24 11:24:57 +0300

    add lists

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from analyzer.analyzer import check_nft_token
 from analyzer.model import NftScamClassifierModel
 from entities.model import NftScamResponse
 from solana_utils.rpc import Endpoint, SolanaRpcClient
-from validation.validation import is_valid_token_length
+from validation.validation import is_valid_token_length, is_in_blacklist, is_in_whitelist
 
 model_file = "/opt/ml/model"
 model = NftScamClassifierModel(model_file, device="cpu")
@@ -15,7 +15,11 @@ def lambda_handler(event, _):
     token_address = event["body"]
     result = NftScamResponse.WRONG_INPUT
 
-    if is_valid_token_length(token_address):
+    if is_in_blacklist(token_address):
+        result = NftScamResponse.SCAM
+    elif is_in_whitelist(token_address):
+        result = NftScamResponse.NOT_SCAM
+    elif is_valid_token_length(token_address):
         result = check_nft_token(model, client, token_address)
 
     return {
--- app/validation/validation.py
+++ app/validation/validation.py
@@ -1,2 +1,13 @@
+from services.nft import blacklist_nft_repository, whitelist_nft_repository
+
+
 def is_valid_token_length(token_address: str) -> bool:
     return len(token_address) == 44
+
+
+def is_in_blacklist(token_address: str) -> bool:
+    return blacklist_nft_repository.exists(token_address)
+
+
+def is_in_whitelist(token_address: str) -> bool:
+    return whitelist_nft_repository.exists(token_address)
