0daf73f2782b8014c6ee760d854a43bd70a53961
Egor <tujh.bazhenov.kbn00@mail.ru>
2023-02-28 19:56:23 +0300

    Revert "fix validation"

--- app/validation/validation.py
+++ app/validation/validation.py
@@ -1,8 +1,8 @@
 from services.nft import blacklist_nft_repository, whitelist_nft_repository
 
 
-def is_valid_token_address(token_address: str) -> bool:
-    return token_address is str and len(token_address) == 44
+def is_valid_token_length(token_address: str) -> bool:
+    return len(token_address) == 44
 
 
 def is_in_blacklist(token_address: str) -> bool:
